ca2d3937d2c537b0e362bb4956783c4b296be084
3podi <riccardo1.tripodi@mail.polimi.it>
2025-11-06 15:37:17 +0100

    added config.save_run

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import wandb
 from omegaconf import DictConfig, OmegaConf
 from datetime import datetime
 
-#from dataloader.audio_loader2 import create_audio_chart_dataloader
 from dataloader.audio_loader import create_chunked_audio_chart_dataloader as create_audio_chart_dataloader
 from dataloader.utils_dataloader import find_audio_files, split_json_entries_by_audio_raw
 from modules.trainer import WaveformTransformerDiscrete
@@ -14,8 +13,6 @@ from lightning.pytorch.callbacks import LearningRateMonitor
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 import hydra
 
-
-#from transformers import AutoProcessor, EncodecModel
 from chart.tokenizer import SimpleTokenizerGuitar
 import json
 
@@ -35,19 +32,9 @@ def main(config: DictConfig):
 
 
     encoder_cfg = config.model.encoder
-    # Compact string for ratios
-    #ratios_str = "-".join(str(r) for r in encoder_cfg.ratios)
-    # Build run name
-    #run_name = (
-    #    f"enc_r{ratios_str}_d{encoder_cfg.dilation_base}"
-    #    f"_l{encoder_cfg.n_residual_layers}_c{encoder_cfg.base_channels}"
-    #    f"_dim{encoder_cfg.dimension}"
-    #)
-
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     run_name = (
         f"{config.model.name}_"
-        #f"{config.root_folder}_"
         f"ws{config.window_seconds}_"
         f"grid{config.grid_ms}_"
         f"seq{config.max_length}_"
@@ -55,7 +42,6 @@ def main(config: DictConfig):
         f"n{config.model.transformer.n_layers}_"
         f"lr{config.optimizer.lr}_"
         f"bs{config.batch_size}_"
-        #f"{run_name}_"
         f"{timestamp}"
     )
 
@@ -76,14 +62,6 @@ def main(config: DictConfig):
         with open(f"{config.data_split_folder}/val.json", "r", encoding="utf-8") as f:
             val_files = json.load(f) 
     else:
-        #find_audio_files(
-        #    root=config.root_folder,
-        #    difficulties=list(config.diff_list),
-        #    instruments=list(config.inst_list),
-        #    output_json=f"{config.root_folder}/audio_dataset.json",
-        #    skipped_json=f"{config.root_folder}/audio_skipped.json",
-        #)
-
         split_json_entries_by_audio_raw(
             input_json=f"{config.root_folder}/audio_dataset_with_raw.json",
             train_json=f"{config.root_folder}/train.json",
@@ -149,22 +127,22 @@ def main(config: DictConfig):
         cfg_model=config.model,
         cfg_optimizer=config.optimizer
     )
-
-    #rf = model.audio_encoder.compute_receptive_field()
-    #wandb.log({"rf": rf})
     
-
     # Callbacks
-    #checkpoint_cb = L.pytorch.callbacks.ModelCheckpoint(
-    #    monitor="val_loss",
-    #    save_top_k=1,
-    #    mode="min",
-    #    filename="best-checkpoint"
-    #)
-
     lr_monitor = LearningRateMonitor(logging_interval='step')
     early_stop_callback = EarlyStopping(monitor="val/acc_nonpad_epoch", min_delta=0.0001, patience=5, verbose=False, mode="max")
     track_grad_norm = LogGradientNorm()
+    
+    callback_list = [lr_monitor, early_stop_callback, track_grad_norm]
+
+    if config.save_run:
+        checkpoint_cb = L.pytorch.callbacks.ModelCheckpoint(
+            monitor="val/acc_nonpad_epoch",
+            save_top_k=1,
+            mode="max",
+            filename="best-checkpoint"
+        )
+        callback_list.append(checkpoint_cb)    
 
     # Trainer
     trainer = L.Trainer(
@@ -172,16 +150,11 @@ def main(config: DictConfig):
         accelerator = "gpu" if config.gpus > 0 else "cpu",
         devices = config.gpus if config.gpus > 0 else 1,
         enable_checkpointing = False,
-        callbacks = [lr_monitor, early_stop_callback, track_grad_norm],
+        callbacks = callback_list,
         log_every_n_steps = 10,
         logger = wandb_logger,
         precision = config.precision,
-        #default_root_dir=checkpoint_path,
-        #check_val_every_n_epoch=cfg["val_frequency"],
         num_sanity_val_steps=0,
-        #limit_train_batches=10,
-        #accumulate_grad_batches=2,
-        #accumulate_grad_batches=cfg.accumulate_grad_batches,
         gradient_clip_val=1.0,
     )
 
